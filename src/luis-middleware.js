da19a7b335e86050fcf41f9f82508fe69b222082
Bruno Nunes <eubruno@me.com>
2016-12-29 03:03:06 -0200

    Support for API v2.0

--- src/luis-middleware.js
+++ src/luis-middleware.js
@@ -9,42 +9,54 @@ module.exports = {
 };
 
 function receive(options) {
-    if (!options || !options.serviceUri) { 
-        throw new Error('No LUIS service url specified.'); 
+    if (!options || !options.serviceUri) {
+        throw new Error('No LUIS service url specified.');
     }
 
     var serviceUri = options.serviceUri.trim();
+	console.log(serviceUri.length);
     if (serviceUri.lastIndexOf('&q=') != serviceUri.length - 3) {
         serviceUri += '&q=';
     }
     var minThreshold = options.minThreshold || 0.1;
     var captureThreshold = options.captureThreshold || 0.7;
     return function (bot, message, next) {
-        // We will only process the text and either there's no topIntent 
+        // We will only process the text and either there's no topIntent
         // or the score for the topIntent is below the captureThreshold.
-        if (message.text && 
+        if (message.text &&
             (!message.topIntent || message.topIntent.score < captureThreshold)) {
             var uri = serviceUri + encodeURIComponent(message.text);
             request.get(uri, function (err, res, body) {
                 try {
                     if (!err) {
-                        // Intents for the builtin Cortana app don't return a score.
+
                         var result = JSON.parse(body);
-                        if (result.intents.length == 1 && !result.intents[0].hasOwnProperty('score')) {
-                            result.intents[0].score = 1.0;
-                        }
-                       
-                        // Find top intent
-                        // - Only return entities for the model with the top intent.
-                        for (var i = 0; i < result.intents.length; i++) {
-                            var intent = result.intents[i];
-                            if (intent.score > minThreshold && 
-                                (!message.topIntent || intent.score > message.topIntent.score)) {
-                                message.topIntent = intent;
-                                message.entities = result.entities || [];
-                                message.action = intent.actions && intent.actions[0].triggered ? intent.actions[0] : null;
-                            }
-                        } 
+
+						if (result.topScoringIntent) {
+							// API v2.0
+							message.topIntent = result.topScoringIntent;
+							message.entities = result.entities || [];
+							message.action = result.topScoringIntent.actions && result.topScoringIntent.actions[0].triggered ? result.topScoringIntent.actions[0] : null;
+						} else {
+							// API v1.0
+
+							// Intents for the builtin Cortana app don't return a score.
+							if (result.intents.length == 1 && !result.intents[0].hasOwnProperty('score')) {
+	                            result.intents[0].score = 1.0;
+	                        }
+
+	                        // Find top intent
+	                        // - Only return entities for the model with the top intent.
+	                        for (var i = 0; i < result.intents.length; i++) {
+	                            var intent = result.intents[i];
+	                            if (intent.score > minThreshold &&
+	                                (!message.topIntent || intent.score > message.topIntent.score)) {
+	                                message.topIntent = intent;
+	                                message.entities = result.entities || [];
+	                                message.action = intent.actions && intent.actions[0].triggered ? intent.actions[0] : null;
+	                            }
+	                        }
+						}
                     } else {
                        console.error(err.toString());
                     }
@@ -55,7 +67,7 @@ function receive(options) {
             });
         } else {
             next();
-        }    
+        }
     };
 }
 
